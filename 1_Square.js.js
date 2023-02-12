f0607b52bcb7396f6d35ec6c52bf5639bf3a1215
ranemayuri <114739560+ranemayuri@users.noreply.github.com>
2023-02-12 19:08:48 +0530

    Add files via upload

---
+++ 1_Square.js.js
@@ -0,0 +1,12 @@
+let a = prompt("Enter first value : ");
+a = Number.parseInt(a);
+let b = prompt("Enter second value : ");
+a = Number.parseInt(b);
+if (a > b)
+{
+  console.log("%d is greater than %d", a, b);
+}
+else 
+{
+  console.log("%d is greater than %d", b, a);
+}
